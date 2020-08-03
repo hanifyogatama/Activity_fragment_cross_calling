93ce69e2993076ef40aebae1138593e6434bb638
Hanif yogatama <hanifyotama@gmail.com>
2020-08-03 22:23:35 +0700

    first commit

---
+++ app/src/main/java/com/binar/activity_fragment_cross_calling/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.binar.activity_fragment_cross_calling
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
